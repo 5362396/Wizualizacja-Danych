d0487b0d9ac1f49e3a30ab9f5aee53c9e374ec67
Arkadiusz Stramko <5362396@wp.pl>
2020-05-26 20:03:37 +0200

    zadania z lekcji 11

--- cw11/zad5.py
+++ cw11/zad5.py
@@ -5,32 +5,32 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 
-fig = plt.figure( figsize =plt.figaspect( 0.5 ))
+fig = plt.figure(figsize=plt.figaspect(0.5))
 
-ax = fig.add_subplot( 1 , 2 , 1 , projection = '3d' )
-X = np.arange(- 5 , 5 , 0.25 )
-Y = np.arange(- 5 , 5 , 0.25 )
+ax = fig.add_subplot(1, 2, 1, projection='3d')
+X = np.arange(- 5, 5, 0.25)
+Y = np.arange(- 5, 5, 0.25)
 X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X** 2 + Y** 2 )
+R = np.sqrt(X ** 2 + Y ** 2)
 Z = np.sin(R)
-surf = ax.plot_surface(X, Y, Z, rstride = 1 , cstride = 1 ,
-cmap = cm.coolwarm,
-linewidth = 0 , antialiased = False )
-ax.set_zlim(- 1.01 , 1.01 )
-fig.colorbar(surf, shrink = 0.5 , aspect = 10 )
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                       cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+ax.set_zlim(- 1.01, 1.01)
+fig.colorbar(surf, shrink=0.5, aspect=10)
 plt.title("Pierwotny wykres")
 
-ax = fig.add_subplot( 1 , 2 , 2 , projection = '3d' )
-X = np.arange(- 5 , 5 , 0.1 )
-Y = np.arange(- 5 , 5 , 0.1 )
+ax = fig.add_subplot(1, 2, 2, projection='3d')
+X = np.arange(- 5, 5, 0.1)
+Y = np.arange(- 5, 5, 0.1)
 X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X** 2 + Y** 2 )
+R = np.sqrt(X ** 2 + Y ** 2)
 Z = np.sin(R)
-surf = ax.plot_surface(X, Y, Z, rstride = 1 , cstride = 1 ,
-cmap = cm.coolwarm,
-linewidth = 0 , antialiased = True )
-ax.set_zlim(- 1.01 , 1.01 )
-fig.colorbar(surf, shrink = 0.5 , aspect = 10 )
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                       cmap=cm.coolwarm,
+                       linewidth=0, antialiased=True)
+ax.set_zlim(- 1.01, 1.01)
+fig.colorbar(surf, shrink=0.5, aspect=10)
 plt.title("Wykres po wprowadzonych zmianach")
 
 plt.show()
